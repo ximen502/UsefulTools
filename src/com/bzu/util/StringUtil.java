f297ac67bb3f974862280d6783c51f68f35a9003
ximen502 <ximen502@126.com>
2019-09-30 01:13:46 +0800

    StringUtil增加一个根据正则表达式匹配目标字符串的方法

--- src/com/bzu/util/StringUtil.java
+++ src/com/bzu/util/StringUtil.java
@@ -1,7 +1,33 @@
 package com.bzu.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by xsc on 2019/6/5.
  */
 public class StringUtil {
+    /**
+     * 在一个字符串中根据指定的正则表达式匹配字符串，将匹配到的字符串放入一个List中。
+     * @param input 字符串
+     * @param regex 正则表达式
+     * @return 匹配到的字符串
+     */
+    private List<String> findStr(String input, String regex) {
+        List<String> list = new ArrayList<>();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()) {
+            //System.out.print("Start index: " + matcher.start());
+            //System.out.print(" End index: " + matcher.end() + " ");
+            String group = matcher.group();
+            //System.out.println(group);
+            list.add(group);
+            //System.out.println(matcher.group(1).replace("'", "\""));
+        }
+
+        return list;
+    }
 }
