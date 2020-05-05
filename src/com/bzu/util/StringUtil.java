c87a3e775ffe58f379cfbb243c13b3414ef9fa34
ximen502 <ximen502@126.com>
2020-05-05 21:51:08 +0800

    StringUtil增加一个拆分长字符串为若干短字符串的方法；类增加单例模式。

--- src/com/bzu/util/StringUtil.java
+++ src/com/bzu/util/StringUtil.java
@@ -9,6 +9,17 @@ import java.util.regex.Pattern;
  * Created by xsc on 2019/6/5.
  */
 public class StringUtil {
+
+    private StringUtil(){}
+
+    private static class SUGet {
+        private static StringUtil instance = new StringUtil();
+    }
+
+    public static StringUtil getInstance(){
+        return SUGet.instance;
+    }
+
     /**
      * 在一个字符串中根据指定的正则表达式匹配字符串，将匹配到的字符串放入一个List中。
      * @param input 字符串
@@ -29,5 +40,35 @@ public class StringUtil {
         }
 
         return list;
+    }
+
+    /**
+     * 将一个长字符串，按固定的长度拆分为若干个短字符串并换行
+     * @param str 长字符串
+     * @param charCount 固定的长度(小于等于长字符串)
+     */
+    public void splitStr(String str, int charCount) {
+        StringBuilder sb = new StringBuilder();
+        int start = 0;
+        int len = charCount <= 0 ? 50 : charCount;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.length() < len) {
+                System.out.println(str.substring(start));
+                break;
+            } else {
+                if (i % len == 0) {
+                    String frag = str.substring(start, i);
+                    sb.append(frag).append("\n");
+                    start = i;
+                    System.out.println(frag);
+                    if (str.length() - start < len) {
+                        String lastFrag = str.substring(start);
+                        System.out.println(lastFrag);
+                        sb.append(lastFrag);
+                    }
+                }
+            }
+        }
+        System.out.println(sb.toString());
     }
 }
